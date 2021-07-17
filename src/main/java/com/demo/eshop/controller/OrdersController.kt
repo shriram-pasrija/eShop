7ceeecd479e177df4191ba1f7b88489b9d7b1d8b
Shriram P <27760040+shriram-pasrija@users.noreply.github.com>
2021-07-17 21:14:46 +0530

    adds basic swagger documentation

--- src/main/java/com/demo/eshop/controller/OrdersController.kt
+++ src/main/java/com/demo/eshop/controller/OrdersController.kt
@@ -3,9 +3,11 @@ package com.demo.eshop.controller
 import com.demo.eshop.dto.request.NewOrderRequestDto
 import com.demo.eshop.dto.response.OrderSummaryResponse
 import com.demo.eshop.service.OrdersService
+import io.swagger.annotations.ApiOperation
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
@@ -18,21 +20,42 @@ class OrdersController {
     @Autowired
     lateinit var ordersService: OrdersService
 
-    @GetMapping(value = ["orders/{orderId}"])
+    @GetMapping(value = ["orders/{orderId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ApiOperation(
+        value = "orders",
+        notes = "Endpoint to get order summary of a given order",
+        response = OrderSummaryResponse::class,
+    )
     fun getOrderSummary(@PathVariable("orderId") orderId: Long): ResponseEntity<OrderSummaryResponse> {
         logger.info("getting order summary for order Id $orderId")
         val orderSummaryResponse = ordersService.getOrderSummary(orderId)
         return ResponseEntity.ok(orderSummaryResponse)
     }
 
-    @GetMapping(value = ["orders"])
+    @GetMapping(value = ["orders"], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ApiOperation(
+        value = "orders",
+        notes = "Endpoint to get order summary for all orders",
+        response = OrderSummaryResponse::class,
+        responseContainer = "List",
+    )
     fun getAllOrdersSummary(): ResponseEntity<List<OrderSummaryResponse>> {
         logger.info("getting order summary for all orders")
         val allOrdersSummary = ordersService.getAllOrdersSummary()
         return ResponseEntity.ok(allOrdersSummary)
     }
 
-    @PostMapping(value = ["orders"])
+    @PostMapping(
+        value = ["orders"],
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    @ApiOperation(
+        value = "orders",
+        notes = "Endpoint to create a new order",
+        response = OrderSummaryResponse::class,
+        responseContainer = "List"
+    )
     fun createNewOrder(
         @RequestBody(required = true) request: NewOrderRequestDto
     ): ResponseEntity<OrderSummaryResponse> {
